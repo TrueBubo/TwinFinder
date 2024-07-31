7e446dec5fcb168bd57d78259397d92f0243314b
Filip Bubak <bubak.filip@pm.me>
2024-07-31 20:15:08 +0200

    Make option parser more modular

--- TwinFinder/Configuration/OptionsParser.cs
+++ TwinFinder/Configuration/OptionsParser.cs
@@ -1,20 +1,30 @@
 using System.Collections;
+using System.Diagnostics;
 
 namespace TwinFinder.Configuration;
 
 public class OptionsParser {
    private static OptionsParser? _instance; // Singleton
    private static readonly object Lock = new object(); // Prevents race condition
-   
+
+
    
    // Default settings
    private Options _options = new Options();
-   public Options options {
-      get { return _options; }
-   }
-   
+   public Options options => _options;
 
-   public OptionsParser(String filename, IConfigReader configReader) {
+   private String[] _filenames;
+
+   public String[] filenames => _filenames;
+   
+   private static readonly Dictionary<string, Action<Hashtable, string, Options>> optionSettingFunctions = new Dictionary<string, Action<Hashtable, string, Options>> {
+           {"mode", (table, nameInTable, options) => options.mode = table[nameInTable]?.ToString() ?? string.Empty},
+           {"normalizeWords", (table, nameInTable, options) => options.normalizeWords = bool.Parse(table[nameInTable]?.ToString() ?? "false")},
+           {"pairsToFind", (table, nameInTable, options) => options.pairsToFind = int.Parse(table[nameInTable]?.ToString() ?? "-1")},
+           {"synonymCount", (table, nameInTable, options) => options.synonymCount = int.Parse(table[nameInTable]?.ToString() ?? "-1")}
+   };
+   
+   public OptionsParser(String filename, String[] args, IConfigReader configReader) {
       lock (Lock) {
          if (_instance != null) return; // Ensures there cannot be more instances
          _instance = this;
@@ -22,21 +32,32 @@ public class OptionsParser {
          // Loads settings from config file
          Hashtable configOptions = configReader.parse(filename);
          
-         try {
-            if (configOptions.ContainsKey("mode")) _options.mode = configOptions["mode"]?.ToString() ?? String.Empty;
-            if (configOptions.ContainsKey("normalizeWords"))
-               _options.normalizeWords =
-                  bool.Parse(configOptions["normalizeWords"]?.ToString() ?? "false");
-            if (configOptions.ContainsKey("pairsToFind"))
-               _options.pairsToFind = int.Parse(configOptions["pairsToFind"]?.ToString() ?? "-1");
-            if (configOptions.ContainsKey("synonymCount"))
-               _options.synonymCount = int.Parse(configOptions["synonymCount"]?.ToString() ?? "-1");
-            if (!options.isValid()) throw new ArgumentException(); // Ensures we do not continue with an invalid config
-         }
-         catch (ArgumentException) {
+         setOptions(configOptions); 
+         // Ensures we do not continue with an invalid config
+         if (!options.isValid()) {
             Console.Error.WriteLine($"There is a mistake in {filename}, please before continuing fix the config file");
             Environment.Exit(1);
          }
+         
       }
+   }
+
+   private bool setOption(Hashtable configOption, String nameInTable, Action<Hashtable, String, Options> update) {
+      if (!configOption.ContainsKey(nameInTable)) return false;
+      update(configOption, nameInTable, _options);
+      return true;
+   }
+   
+   private void setOptions(Hashtable configOptions) {
+      foreach (var pair in optionSettingFunctions) {
+         if (setOption(configOptions, pair.Key, pair.Value)) configOptions.Remove(pair.Key);
+      }
+
+      foreach (String key in configOptions.Keys) {
+         Console.Error.WriteLine($"Unknown option {key}");
+      }
+      if (configOptions.Count > 0) Environment.Exit(1);
+      
+      Console.WriteLine($"{options.mode} {options.normalizeWords} {options.synonymCount} {options.pairsToFind}");
    }
 }
