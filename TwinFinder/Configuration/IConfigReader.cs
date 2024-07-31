d21901dba05d7014b828fd068a7a4489d5f75797
Filip Bubak <bubak.filip@pm.me>
2024-07-31 23:44:52 +0200

    Frequencies padding

--- TwinFinder/Configuration/IConfigReader.cs
+++ TwinFinder/Configuration/IConfigReader.cs
@@ -3,5 +3,5 @@ using System.Collections;
 namespace TwinFinder.Configuration;
 
 public interface IConfigReader {
-    public Hashtable parse(String filename);
+    public Hashtable parse(String loc);
 }
--- TwinFinder/ContentAnalysis/ProcessContent.cs
+++ TwinFinder/ContentAnalysis/ProcessContent.cs
@@ -4,25 +4,34 @@ using TwinFinder.ContentIO;
 namespace TwinFinder.ContentAnalysis;
 
 public class ProcessContent {
-    ConcurrentDictionary<String, Dictionary<String, int>> _frequencies = new ConcurrentDictionary<String, Dictionary<string, int>>();
-	ConcurrentBag<String> _uniqueWords = new ConcurrentBag<String>();
+    private ConcurrentDictionary<String, Dictionary<String, int>> _frequencies = new ConcurrentDictionary<String, Dictionary<string, int>>();
+	private ConcurrentBag<String> _uniqueWords = new ConcurrentBag<String>();
 	
-	
-	public void processContent(String filename, IWordsParser parser, String mode, bool normalizeWords) {
-		String[] words = parser.parse(filename, normalizeWords);
-		
+	public void processContent(String loc, IWordsParser parser, String mode, bool normalizeWords) {
+		String[] words = parser.parse(loc, normalizeWords);
+
 		switch (mode) {
 			case "closest": {
 				Dictionary<String, int> frequenciesFile = TextAnalyzer.getFrequencies(words);
-				_frequencies[filename] = frequenciesFile;
+				_frequencies[loc] = frequenciesFile;
 				foreach (String word in frequenciesFile.Keys) {
 					_uniqueWords.Add(word);
 				}
-				break; 
+				
+				break;
 			}
 			default: {
-				throw new ArgumentException($"Mode {mode} does not exist");
+				Console.Error.WriteLine($"Mode {mode} does not exist");
+				Environment.Exit(1);
+				break;
 			}
+		}
 	}
+
+	// Pads frequencies with 0s, so every file contains the same keys to make comparing easier
+	public void padFrequencies(String loc) {
+		foreach (string word in _uniqueWords) {
+			if (!_frequencies[loc].ContainsKey(word)) _frequencies[loc][word] = 0;
+		}
 	}
 }
