497b3b32e0a21094fe3ce4d4b5fd8fbf0f632fc4
Filip Bubak <bubak.filip@pm.me>
2024-08-02 13:46:45 +0200

    Add file with defaults

---
+++ TwinFinder/Configuration/Defaults.cs
@@ -0,0 +1,8 @@
+namespace TwinFinder.Configuration;
+
+static class Defaults {
+    public const String Mode = "closest";
+    public const bool NormalizeWords = false;
+    public const int PairsToFind = 3;
+    public const int SynonymCount = 5;
+}
